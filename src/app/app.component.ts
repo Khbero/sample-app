83db7087f14c7d3d9bb5aeeefa8701287bee6147
Khbero Baker <khbero.baker@flosports.tv>
2021-08-20 09:45:20 -0500

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import algoliasearch from 'algoliasearch';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit, AfterViewInit {
+  config = {
+    searchClient: algoliasearch('B1G2GM9NG0', 'aadef574be1f9252bb48d4ea09b5cfe5'),
+    indexName: 'demo_ecommerce',
+    routing: true
+  };
+
+  ngOnInit(): void {
+    window.addEventListener('keyup', this.onKeyUp);
+    window.addEventListener('click', this.onClick);
+  }
+
+  ngAfterViewInit(): void {
+    
+  }
+
+  onKeyUp = (event: any) => {
+    console.log(event)
+  };
+
+  onClick = (event: any) => {
+    console.log(event)
+  };
+
 }
