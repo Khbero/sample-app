3eb5e3d880b4137688ef1e5f136b58c4867ef740
Khbero Baker <khbero.baker@flosports.tv>
2021-08-25 15:25:46 -0500

    undo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -8,9 +7,19 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-      imports: [AppModule]
     }).compileComponents();
   });
 
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
 
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('my-app app is running!');
+  });
 });
