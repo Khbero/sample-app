376f68cc17c251164ed1ace7ddd070d5ccb48440
Khbero Baker <khbero.baker@flosports.tv>
2021-08-27 10:02:41 -0500

    more changes made

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
   },
+  transformIgnorePatterns: ['node_modules/(?!(instantsearch.js))'],
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
 };
